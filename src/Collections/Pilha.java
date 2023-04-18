eca7a03bf90e2c2e8762e54e950dd0b8394c00de
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-18 20:02:45 -0300

    stack

---
+++ src/Collections/Pilha.java
@@ -0,0 +1,25 @@
+package Collections;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+    public static void main(String[] args) {
+
+        Deque<String> livros = new ArrayDeque<String>();
+
+        livros.add("O Pequeno Principe");
+        livros.push("Don Quixote");
+        livros.push("O Hobbit");
+
+        System.out.println(livros.peek());
+        System.out.println(livros.element());
+
+        System.out.println(livros.pop());
+        System.out.println(livros.poll());
+        System.out.println(livros.poll());
+        System.out.println(livros.poll());
+
+
+    }
+}
