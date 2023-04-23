ca6f7ab59eaf0ac27d15a97c6416593129c086bb
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-22 23:30:57 -0300

    POO

---
+++ src/OrientacaoObjeto/Exercicios/Desafio/Item.java
@@ -0,0 +1,14 @@
+package OrientacaoObjeto.Exercicios.Desafio;
+
+public class Item {
+
+   final Produto produto;
+    final int quantidade;
+
+    Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+
+}
---
+++ src/OrientacaoObjeto/Exercicios/Desafio/Produto.java
@@ -0,0 +1,13 @@
+package OrientacaoObjeto.Exercicios.Desafio;
+
+public class Produto {
+
+    String nome;
+    double preco;
+
+    Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+}
